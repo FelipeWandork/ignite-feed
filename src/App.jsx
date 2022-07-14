c7ff39cdc3c3cc4e21ff36fb5c5205348748de8c
Felipe Wandork <wandork@gmail.com>
2022-07-14 15:23:39 -0300

    Blog com publicação configurada

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,36 @@ import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/FelipeWandork.png',
+      name: 'Felipe Wandork',
+      role: 'Tech Lead'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto para o GH e estou divulgando por aqui' },
+      { type: 'link', content: 'felipewandork@gmail.com' },
+    ],
+    publishedAt: new Date('2022-07-14 09:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayke Brito',
+      role: 'Instructor'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal' },
+      { type: 'paragraph', content: 'Subi mais um projeto para o GH e estou divulgando por aqui' },
+      { type: 'link', content: 'felipewandork@gmail.com' },
+    ],
+    publishedAt: new Date('2022-07-13 09:00:00'),
+  }
+]
 
 export function App() {
   return (
@@ -11,7 +41,18 @@ export function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <Post />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+
+        </main>
       </div>
     </div>
   )
